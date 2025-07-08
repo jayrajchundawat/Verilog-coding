0359d834fede2e1ac6489ce2d1e680db43fbcd91
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-08 23:10:09 +0530

    Update tb_full_add_behavioural.v

--- combinational design/tb_full_add_behavioural.v
+++ combinational design/tb_full_add_behavioural.v
@@ -1,19 +1,19 @@
 module tb_full_add_behavioral();
-       reg  a;
+           reg  a;
 	   reg b;
 	   reg carry_in;
 	   wire sum;
 	   wire carry_out;
 	   
 	   full_add_behavioral FULL_ADD3 (
-	       .a(a),
+	           .a(a),
 		   .b(b),
 		   .carry_in(carry_in),
 		   .sum(sum),
 		   .carry_out(carry_out)
 		   );
 		   
-	   initial begin
+       initial begin
          $monitor("a=%b, b=%b, carry_in=%0b, sum=%b, carry_out =%b", a, b, carry_in, sum, carry_out);
        end  
   
