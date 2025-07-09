b0b75eddfcd03c1ba4e448dcb331571964cbfdec
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-09 20:45:15 +0530

    Update encoder_8to3.v

--- combinational design/encoder_8to3.v
+++ combinational design/encoder_8to3.v
@@ -1,5 +1,5 @@
 module encoder_8to3 (
-       input [7:0] d,
+           input [7:0] d,
 	   input enable,
 	   output reg [2:0] y
 	   );
@@ -9,7 +9,7 @@ module encoder_8to3 (
 		    y=8'b0;
 		 else begin
 		   case (d)
-		      8'b00000000 : y = 3'd0;
+		          8'b00000000 : y = 3'd0;
 			  8'b00000010 : y = 3'd1;
 			  8'b00000100 : y = 3'd2;
 			  8'b00001000 : y = 3'd3;
@@ -19,6 +19,6 @@ module encoder_8to3 (
 			  8'b10000000 : y = 3'd7;
 			  default y = 3'd0;
 		   endcase
-		  end
+		end
 	   end
 endmodule
