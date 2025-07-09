9136cfca84faed034146cdcf8d34f5cc0ef666c0
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-09 20:51:22 +0530

    Update tb_ripple_add_4bit_behavioral.v

--- combinational design/tb_ripple_add_4bit_behavioral.v
+++ combinational design/tb_ripple_add_4bit_behavioral.v
@@ -1,5 +1,5 @@
 module tb_ripple_add_4bit_behavioral();
-        reg [3:0] a;
+                reg [3:0] a;
 		reg [3:0] b;
 		reg carry_in;
 		wire [3:0] sum;
@@ -8,7 +8,6 @@ module tb_ripple_add_4bit_behavioral();
 		wire [2:0] c;
 		
 		ripple_add_4bit_behavioral RIP_ADD(
-		
 		  .a(a),
 		  .b(b),
 		  .carry_in(carry_in),
@@ -17,12 +16,12 @@ module tb_ripple_add_4bit_behavioral();
 		  );
 		  
 		  
-		initial begin
+	      initial begin
 		 $monitor ("a=%b,b=%b,carry_in=%b,sum=%b,carry_out=%b",a,b,carry_in,sum,carry_out);
 		 #1; a=4'b0000; b=4'b0000; carry_in=0;
 		 #1; a = 4'b0000; b = 4'b0000; carry_in = 1;
-         #1; a = 4'b0001; b = 4'b0001; carry_in = 0;
-         #1; a = 4'b0001; b = 4'b0001; carry_in = 1;
-         #1; a = 4'd3;    b = 4'd6;    carry_in = 0;
-		end
+                 #1; a = 4'b0001; b = 4'b0001; carry_in = 0;
+                 #1; a = 4'b0001; b = 4'b0001; carry_in = 1;
+                 #1; a = 4'd3;    b = 4'd6;    carry_in = 0;
+	      end
 endmodule
