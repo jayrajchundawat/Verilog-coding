7a1cfc636eeebfb4f765bd47f2799405d8f437be
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-09 20:42:09 +0530

    encoder_8to3

---
+++ combinational design/encoder_8to3.v
@@ -0,0 +1,24 @@
+module encoder_8to3 (
+       input [7:0] d,
+	   input enable,
+	   output reg [2:0] y
+	   );
+	   
+	   always @(*) begin
+	     if (enable==0)
+		    y=8'b0;
+		 else begin
+		   case (d)
+		      8'b00000000 : y = 3'd0;
+			  8'b00000010 : y = 3'd1;
+			  8'b00000100 : y = 3'd2;
+			  8'b00001000 : y = 3'd3;
+			  8'b00010000 : y = 3'd4;
+			  8'b00100000 : y = 3'd5;
+			  8'b01000000 : y = 3'd6;
+			  8'b10000000 : y = 3'd7;
+			  default y = 3'd0;
+		   endcase
+		  end
+	   end
+endmodule
---
+++ combinational design/tb_encoder_8to3.v
@@ -0,0 +1,26 @@
+`timescale 1ns/100ps
+ module tb_encoder_8to3 ();
+         reg [7:0] d;
+		 reg enable;
+		 wire [2:0] y;
+		 
+		 encoder_8to3 END_8to3 (
+		    .d(d),
+			.enable(enable),
+			.y(y)
+			);
+			
+		initial begin 
+		  $monitor ("d=%b,enable=%d,y=%d",d,enable,y);
+		   d=0; enable=0;
+		   #1; d=8'b00000000; enable=1;
+		   #1; d=8'b00000010; enable=1;
+		   #1; d=8'b00000100; enable=1;
+		   #1; d=8'b00001000; enable=1;
+		   #1; d=8'b00010000; enable=1;
+		   #1; d=8'b00100000; enable=1;
+		   #1; d=8'b01000000; enable=1;
+		   #1; d=8'b10000000; enable=1;
+		   #1; $stop;
+		end
+endmodule
