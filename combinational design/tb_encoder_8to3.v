e5da79171e023f8c4c337221b901094cf9a0759d
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-09 20:50:03 +0530

    Update tb_encoder_8to3.v

--- combinational design/tb_encoder_8to3.v
+++ combinational design/tb_encoder_8to3.v
@@ -1,17 +1,17 @@
 `timescale 1ns/100ps
  module tb_encoder_8to3 ();
-         reg [7:0] d;
+                 reg [7:0] d;
 		 reg enable;
 		 wire [2:0] y;
 		 
 		 encoder_8to3 END_8to3 (
-		    .d(d),
+		        .d(d),
 			.enable(enable),
 			.y(y)
 			);
 			
 		initial begin 
-		  $monitor ("d=%b,enable=%d,y=%d",d,enable,y);
+		   $monitor ("d=%b,enable=%d,y=%d",d,enable,y);
 		   d=0; enable=0;
 		   #1; d=8'b00000000; enable=1;
 		   #1; d=8'b00000010; enable=1;
