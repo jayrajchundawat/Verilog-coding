ecebf783dd07bdcf0a073a765fa2c59ebb9214a0
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-10 22:03:55 +0530

    Update Hex_7seg_decoder.v

--- combinational design/Hex_7seg_decoder.v
+++ combinational design/Hex_7seg_decoder.v
@@ -1,5 +1,5 @@
 module Hex_7seg_decoder (
-       input [3:0] in,
+           input [3:0] in,
 	   output reg a,
 	   output reg b,
 	   output reg c,
@@ -13,9 +13,9 @@ module Hex_7seg_decoder (
 	always @ (*) begin
 	  case (in)
 		4'd0: {a,b,c,d,e,f,g} = 7'b1111110;
-	    4'd1: {a,b,c,d,e,f,g} = 7'b0110000;
-	    4'd2: {a,b,c,d,e,f,g} = 7'b1101101;
-	    4'd3 : {a,b,c,d,e,f,g} = 7'b1111001;
+	        4'd1: {a,b,c,d,e,f,g} = 7'b0110000;
+	        4'd2: {a,b,c,d,e,f,g} = 7'b1101101;
+	        4'd3 : {a,b,c,d,e,f,g} = 7'b1111001;
 		4'd4 : {a,b,c,d,e,f,g} = 7'b0110011;
 		4'd5 : {a,b,c,d,e,f,g} = 7'b1011011;  
 		4'd6 : {a,b,c,d,e,f,g} = 7'b1011111;
