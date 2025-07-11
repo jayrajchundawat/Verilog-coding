5a883261d6b66c6d70626453468c70cb51580c9a
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-11 21:12:59 +0530

    comparator

---
+++ combinational design/comparator.v
@@ -0,0 +1,16 @@
+module comparator (
+      input a,
+	  input b,
+	  output smaller,
+	  output equal,
+	  output greater
+	  );
+	  
+	  wire not_a;
+	  wire not_b;
+	  
+	  and and1 (smaller,not_a,b);
+	  nxor nxor1 (equal,a,b);
+	  and and2 (greater,not_b,a);
+	  
+endmodule
