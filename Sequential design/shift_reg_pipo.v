ecf9e83049ff79c41d2988aed4ee8996f239368d
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-15 22:49:48 +0530

    Update shift_reg_pipo.v

--- Sequential design/shift_reg_pipo.v
+++ Sequential design/shift_reg_pipo.v
@@ -1,15 +1,15 @@
 module shift_reg_pipo(
 	input reset_n,
 	input clk,
-    input [3:0] d,
+        input [3:0] d,
 	output reg [3:0] q
     );
 	
 	always @(posedge clk or negedge reset_n) begin
 	    if (!reset_n)
-		    q <= 4'b0;
+		q <= 4'b0;
 	    else
-		    q[3:0] <= d[3:0];  
+		q[3:0] <= d[3:0];  
 	end
 
 endmodule
