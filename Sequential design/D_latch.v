9a8390f6a8b9cb044552c0daa9c540f42837fd71
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-15 21:29:21 +0530

    D_latch

---
+++ Sequential design/D_latch.v
@@ -0,0 +1,18 @@
+module D_latch (
+        input d,
+		input enable,
+		output q,
+		output q_not
+		);
+		
+		reg d_latch;
+		
+		always@ (enable or d) begin
+		   if (enable)
+		     d_latch <= d;
+		end
+		
+		assign q = d_latch;
+		assign q_not = ~q;
+		
+endmodule
