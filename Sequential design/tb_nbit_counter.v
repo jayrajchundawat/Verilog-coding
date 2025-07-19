cd5332cd37dba68080edc64e6ce1a7bf1caff7e0
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-19 12:55:27 +0530

    Update tb_nbit_counter.v

--- Sequential design/tb_nbit_counter.v
+++ Sequential design/tb_nbit_counter.v
@@ -1,14 +1,14 @@
 `timescale 1ns/1ps
 module tb_nbit_counter ();
-     parameter CNT_WIDTH = 3;
+         parameter CNT_WIDTH = 3;
 	 reg clk=0;
 	 reg reset_n;
 	 wire [CNT_WIDTH-1:0] counter;
 	 
 	 nbit_counter 
 	  #(.CNT_WIDTH(CNT_WIDTH))
-	  CNT1 (
-	         .clk(clk),
+	          CNT1 (
+	                 .clk(clk),
 			 .reset_n(reset_n),
 			 .counter (counter)
 			);
