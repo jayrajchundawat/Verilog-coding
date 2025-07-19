3aa515093b0b4d8faff584db976b2c769496adf9
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-19 11:53:12 +0530

    LFSR

---
+++ Sequential design/lfsr_16.v
@@ -0,0 +1,23 @@
+
+module lfsr_16 (
+       input clk,
+	   input reset_n,
+	   input enable,
+	   output reg [15:0]lfsr
+	   );
+	   
+	   localparam RST_SEED = 16'h1001;
+	   wire feedback;
+	   
+	   assign feedback = lfsr[15] ^ lfsr[13] ^ lfsr[12] ^ lfsr[10];
+	   
+	   always @ (posedge clk or negedge reset_n) begin
+	      if (!reset_n)
+		     lfsr <= RST_SEED;
+		  else if (enable == 1'b1)
+		     lfsr <= {lfsr [14:0],feedback};
+		end
+endmodule
+		  
+	   
+	   
---
+++ Sequential design/tb_lfsr_16.v
@@ -0,0 +1,38 @@
+`timescale 1ns/1ps
+module tb_lfsr_16 ();
+   
+   reg clk=0;
+   reg enable;
+   reg reset_n;
+   wire [15:0] lfsr;
+   
+   lfsr_16  LFSR16 (
+      .clk(clk),
+	  .enable(enable),
+	  .reset_n(reset_n),
+	  .lfsr(lfsr)
+	  );
+	  
+	always begin
+	 #0.5; clk = ~clk;
+	end
+	  
+	  initial begin
+	   $monitor ($time,"enable = %d, lfsr = 0x%x", enable, lfsr);
+	   
+	   #1; reset_n=0; enable=0;
+	   
+	   #1.3; reset_n=1; 
+	   repeat(2) @ (posedge clk);
+	   
+	   enable=1;
+	   repeat (10) @ (posedge clk);
+	   
+	   enable=0;
+	   end
+	
+    initial begin
+        #20 $stop;
+    end  
+endmodule
+	   
