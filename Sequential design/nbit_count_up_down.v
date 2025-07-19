a8abbe01e76b52be1b8879ff53ee615f4a5e6cc4
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-19 12:53:29 +0530

    count_up_down

---
+++ Sequential design/nbit_count_up_down.v
@@ -0,0 +1,18 @@
+module nbit_count_up_down 
+      # (parameter CNT_WIDTH = 3)
+      (
+        input clk,
+		input reset_n,
+		input count_up_down,
+		output reg [CNT_WIDTH-1:0] counter);
+		
+		always @ (posedge clk or negedge reset_n) begin
+		    if (!reset_n)
+			  counter <= 0;
+			else if (count_up_down == 1'b1) begin 
+			  counter <= counter + 1'b1;
+			end else begin
+			  counter <= counter - 1'b1; 
+			end
+		end
+endmodule
---
+++ Sequential design/tb_nbit_count_up_down.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ps
+module tb_nbit_count_up_down ();
+     parameter CNT_WIDTH = 3;
+	 reg clk=0;
+	 reg reset_n;
+	 reg count_up_down;
+	 wire [CNT_WIDTH-1:0] counter;
+	 
+	 nbit_count_up_down 
+	  #(.CNT_WIDTH(CNT_WIDTH))
+	  CNTUD (
+	         .clk(clk),
+			 .reset_n(reset_n),
+			 .count_up_down(count_up_down),
+			 .counter (counter)
+			);
+			
+	 always begin 
+	   #0.5; clk=~clk;
+	 end
+	 
+	 initial begin
+	   $monitor ($time,"counter = %d", counter);
+	   #1; reset_n =0; 
+	   #1.3; reset_n= 1; count_up_down = 1'b1;
+	   #4; reset_n = 1; count_up_down = 1'b0;
+	   #20; $stop;
+	 end
+endmodule
+    
+	
