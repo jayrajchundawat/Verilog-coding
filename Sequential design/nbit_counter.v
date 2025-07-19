d799c92c78edcf3bd85ba1992f3dfceffc203f87
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-19 12:54:35 +0530

    Update nbit_counter.v

--- Sequential design/nbit_counter.v
+++ Sequential design/nbit_counter.v
@@ -1,14 +1,14 @@
 module nbit_counter 
       # (parameter CNT_WIDTH = 3)
-      (
-        input clk,
+               (
+                input clk,
 		input reset_n,
 		output reg [CNT_WIDTH-1:0] counter);
 		
 		always @ (posedge clk or negedge reset_n) begin
 		    if (!reset_n)
 			  counter <= 0;
-			else  
+		    else  
 			  counter <= counter + 1'b1;
 		end
 endmodule
