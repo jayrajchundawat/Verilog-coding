f256ce1bfa25b2b95bf602c736cb1b43f9c68e7e
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-19 10:45:56 +0530

    Update tb_shift_left_right_reg.v

--- Sequential design/tb_shift_left_right_reg.v
+++ Sequential design/tb_shift_left_right_reg.v
@@ -1,13 +1,11 @@
 `timescale 1us/1ns
-
 module tb_shift_left_right_reg();
 	
-	
-    reg clk = 0;
+        reg clk = 0;
 	reg reset_n;
 	reg [7:0] i;         
 	reg load_enable; 
-    reg shift_left_right;
+        reg shift_left_right;
 	wire [7:0] q;
 
     shift_left_right_reg SRL0(
@@ -15,9 +13,9 @@ module tb_shift_left_right_reg();
 	    .reset_n(reset_n),
 	    .i(i),         
 	    .load_enable(load_enable),     
-        .shift_left_right(shift_left_right),
+            .shift_left_right(shift_left_right),
 	    .q(q)
-    );
+          );
 	
 	always begin
 	    #0.5 clk = ~clk;
@@ -27,19 +25,18 @@ module tb_shift_left_right_reg();
 	    $monitor($time," i=%8b, load_enable=%1b, shift_left_right=%1b, q=%8b", 
 		               i, load_enable, shift_left_right, q);
 
-	    #1; 
-        reset_n = 0; i = 0; load_enable = 0; shift_left_right = 0;		
+	        #1; 
+                reset_n = 0; i = 0; load_enable = 0; shift_left_right = 0;		
 		#1.3; reset_n = 1; 
 		
 		@(posedge clk); i = 8'b1111_1111; 
 		@(posedge clk); load_enable = 1'b1; 
-       
-        repeat (5) @(posedge clk); 
-        @(posedge clk); load_enable = 1'b0;	i = 8'b1010_1000;	
-		@(posedge clk); load_enable = 1'b1; shift_left_right = 1;
-        repeat (5) @(posedge clk); 
-		@(posedge clk); 
-	end
+                repeat (5) @(posedge clk); 
+                @(posedge clk); load_enable = 1'b0;	i = 8'b1010_1000;	
+	        @(posedge clk); load_enable = 1'b1; shift_left_right = 1;
+                repeat (5) @(posedge clk); 
+	        @(posedge clk); 
+    end
 	
     initial begin
         #40 $finish;
