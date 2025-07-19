4ed85f8ec2f14ee9b0d0aabb4474a28cabd03ed3
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-19 12:35:36 +0530

    nbit_counter

---
+++ Sequential design/nbit_counter.v
@@ -0,0 +1,14 @@
+module nbit_counter 
+      # (parameter CNT_WIDTH = 3)
+      (
+        input clk,
+		input reset_n,
+		output reg [CNT_WIDTH-1:0] counter);
+		
+		always @ (posedge clk or negedge reset_n) begin
+		    if (!reset_n)
+			  counter <= 0;
+			else  
+			  counter <= counter + 1'b1;
+		end
+endmodule
---
+++ Sequential design/tb_nbit_counter.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ps
+module tb_nbit_counter ();
+     parameter CNT_WIDTH = 3;
+	 reg clk=0;
+	 reg reset_n;
+	 wire [CNT_WIDTH-1:0] counter;
+	 
+	 nbit_counter 
+	  #(.CNT_WIDTH(CNT_WIDTH))
+	  CNT1 (
+	         .clk(clk),
+			 .reset_n(reset_n),
+			 .counter (counter)
+			);
+			
+	 always begin 
+	   #0.5; clk=~clk;
+	 end
+	 
+	 initial begin
+	   $monitor ($time,"counter = %d", counter);
+	   #1; reset_n =0; 
+	   #1.3; reset_n= 1;
+	   #20; $stop;
+	 end
+endmodule
+    
+	
