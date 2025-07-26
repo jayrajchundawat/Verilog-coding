b522a7fd9226542e7c1d11382838671475f1d95d
jayrajchundawat <JAY.868.RAJ@GMAIL.COM>
2025-07-26 12:09:45 +0530

    Update tb_Seq_det_non_overlap.v

--- Finite State Machine/tb_Seq_det_non_overlap.v
+++ Finite State Machine/tb_Seq_det_non_overlap.v
@@ -1,28 +1,28 @@
 `timescale 1ns/1ps
 module tb_Seq_det_non_overlap();
 
-    reg clk = 0;
+        reg clk = 0;
 	reg reset_n;
 	reg seq_in;
 	wire detected;
 	wire [1:0] state_out;
    
-    reg [0:13] test_vect = 14'b00_1100_0101_0101;
-	integer i;
+         reg [0:13] test_vect = 14'b00_1100_0101_0101;
+	 integer i;
 	
     
     Seq_det_non_overlap SEQ_DET0(
-        .clk      (clk      ),
-		.reset_n    (reset_n    ),
-		.seq_in   (seq_in   ),
+                .clk      (clk ),
+		.reset_n  (reset_n ),
+		.seq_in   (seq_in  ),
 		.detected (detected ),
 		.state_out(state_out)
-        );
+            );
 	
      
         always begin 
 		    #1 clk = ~clk;
-	    end
+	end
    
 
     initial begin
@@ -33,19 +33,19 @@ module tb_Seq_det_non_overlap();
 		
 		for(i=0; i<14; i=i+1) begin
 		    seq_in = test_vect[i];
-			@(posedge clk);
+		    @(posedge clk);
 		end
 		
 		for(i=0; i<15; i=i+1) begin
 		    seq_in = $random;
-			@(posedge clk);
+		    @(posedge clk);
 		end
 		
 	
 	    repeat(10) @(posedge clk); 
-        @(posedge clk);
+            @(posedge clk);
 
 	    #40 $stop;
-	end   
+     end   
     
 endmodule
